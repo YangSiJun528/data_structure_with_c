809d9561c2ea0f1c7c31edf082881a4d6dc49c65
Sijun Yang <yangsijun5528@gmail.com>
2024-05-30 18:32:50 +0900

    Fix: log_write_to_file.h 잘못된 파일로 덮어씌워진 문제

--- include/log_write_to_file.h
+++ include/log_write_to_file.h
@@ -1,1 +1,11 @@
-To upload log directory to github repository
+//
+// Created by sijun yang on 2024. 5. 26.
+//
+
+#ifndef LOG_READER
+#define LOG_READER
+extern char* LOG_DIRECTORY;
+
+int save_log(char* subsystem);
+
+#endif //LOG_READER
