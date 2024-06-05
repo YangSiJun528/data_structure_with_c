49b65dc8ac4a3b63213cc18adb48f4d3c077b841
Sijun Yang <yangsijun5528@gmail.com>
2024-06-05 17:12:47 +0900

    Style: 스네이크 케이스로 통일

--- include/doubly_linked_list.h
+++ include/doubly_linked_list.h
@@ -4,29 +4,42 @@
 
 #ifndef DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
 #define DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
-#define TYPE int // 자료구조의 요소 타입
+#define TYPE Data // 자료구조의 요소 타입, 구조체나 배열만 가능. 포인터를 사용하는 방식으로 일관성있게 처리할거라 그럼. void* 도 가능하긴 한데, 그럼 포인터 다루기 까다로워서...
+
+typedef struct {
+    int value;
+} Data;
 
 typedef struct Node {
     TYPE value;         // 노드가 저장한 값
-    struct Node* next;  // 다음 노드를 가리키는 포인터
-    struct Node* prev;  // 이전 노드를 가리키는 포인터
+    struct Node *next;  // 다음 노드를 가리키는 포인터
+    struct Node *prev;  // 이전 노드를 가리키는 포인터
 } Node;
 
 typedef struct {
-    Node* head;     // 처음 노드
-    Node* tail;     // 마지막 노드
+    Node *head;     // 처음 노드
+    Node *tail;     // 마지막 노드
     int length;     // 배열 길이
 } LinkedList;
 
 LinkedList linkedlist_create();
-void linkedlist_push(LinkedList * linkedList, TYPE element);
-TYPE linkedlist_pop(LinkedList * linkedList);
-void linkedlist_pushleft(LinkedList * linkedList, TYPE element);
-TYPE linkedlist_popleft(LinkedList * linkedList);
-TYPE linkedlist_get(LinkedList * linkedList, int index);
-void linkedlist_insert(LinkedList * linkedList, TYPE element);
-void linkedlist_update(LinkedList * linkedList, TYPE element);
-TYPE linkedlist_delete(LinkedList * linkedList);
-int linkedlist_length(LinkedList * linkedList);
+
+void linkedlist_push(LinkedList *linked_list, TYPE element);
+
+TYPE linkedlist_pop(LinkedList *linked_list);
+
+void linkedlist_pushleft(LinkedList *linked_list, TYPE element);
+
+TYPE linkedlist_popleft(LinkedList *linked_list);
+
+TYPE linkedlist_get(LinkedList *linked_list, int index);
+
+void linkedlist_insert(LinkedList *linked_list, TYPE element);
+
+void linkedlist_update(LinkedList *linked_list, TYPE element);
+
+TYPE linkedlist_delete(LinkedList *linked_list);
+
+int linkedlist_length(LinkedList *linked_list);
 
 #endif //DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
--- src/doubly_linked_list.c
+++ src/doubly_linked_list.c
@@ -2,24 +2,32 @@
 // Created by sijun yang on 2024. 6. 5..
 //
 
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "doubly_linked_list.h"
+
+static inline Node *create_node();
 
 LinkedList linkedlist_create() {}
 
-void linkedlist_push(LinkedList *linkedList, TYPE element) {}
+void linkedlist_push(LinkedList *linked_list, TYPE element) {}
 
-TYPE linkedlist_pop(LinkedList *linkedList) {}
+TYPE linkedlist_pop(LinkedList *linked_list) {}
 
-void linkedlist_pushleft(LinkedList *linkedList, TYPE element) {}
+void linkedlist_pushleft(LinkedList *linked_list, TYPE element) {}
 
-TYPE linkedlist_popleft(LinkedList *linkedList) {}
+TYPE linkedlist_popleft(LinkedList *linked_list) {}
 
-TYPE linkedlist_get(LinkedList *linkedList, int index) {}
+TYPE linkedlist_get(LinkedList *linked_list, int index) {}
 
-void linkedlist_insert(LinkedList *linkedList, TYPE element) {}
+void linkedlist_insert(LinkedList *linked_list, TYPE element) {}
 
-void linkedlist_update(LinkedList *linkedList, TYPE element) {}
+void linkedlist_update(LinkedList *linked_list, TYPE element) {}
 
-TYPE linkedlist_delete(LinkedList *linkedList) {}
+TYPE linkedlist_delete(LinkedList *linked_list) {}
 
-int linkedlist_length(LinkedList *linkedList) {}
+int linkedlist_length(LinkedList *linked_list) {}
+
+
+static inline Node *create_node() {};
