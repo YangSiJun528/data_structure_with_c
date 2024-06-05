9629821ae4784307bb3006fc1e6b526cf9ef98da
Sijun Yang <yangsijun5528@gmail.com>
2024-06-05 12:09:11 +0900

    Feat: 양방향 연결 리스트 정의

---
+++ src/doubly_linked_list.c
@@ -0,0 +1,25 @@
+//
+// Created by sijun yang on 2024. 6. 5..
+//
+
+#include "doubly_linked_list.h"
+
+LinkedList linkedlist_create() {}
+
+void linkedlist_push(LinkedList *linkedList, TYPE element) {}
+
+TYPE linkedlist_pop(LinkedList *linkedList) {}
+
+void linkedlist_pushleft(LinkedList *linkedList, TYPE element) {}
+
+TYPE linkedlist_popleft(LinkedList *linkedList) {}
+
+TYPE linkedlist_get(LinkedList *linkedList, int index) {}
+
+void linkedlist_insert(LinkedList *linkedList, TYPE element) {}
+
+void linkedlist_update(LinkedList *linkedList, TYPE element) {}
+
+TYPE linkedlist_delete(LinkedList *linkedList) {}
+
+int linkedlist_length(LinkedList *linkedList) {}
---
+++ src/doubly_linked_list.h
@@ -0,0 +1,32 @@
+//
+// Created by sijun yang on 2024. 6. 5.
+//
+
+#ifndef DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
+#define DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
+#define TYPE int // 자료구조의 요소 타입
+
+typedef struct Node {
+    TYPE value;         // 노드가 저장한 값
+    struct Node* next;  // 다음 노드를 가리키는 포인터
+    struct Node* prev;  // 이전 노드를 가리키는 포인터
+} Node;
+
+typedef struct LinkedList {
+    Node* head;     // 처음 노드
+    Node* tail;     // 마지막 노드
+    int length;     // 배열 길이
+} LinkedList;
+
+LinkedList linkedlist_create();
+void linkedlist_push(LinkedList * linkedList, TYPE element);
+TYPE linkedlist_pop(LinkedList * linkedList);
+void linkedlist_pushleft(LinkedList * linkedList, TYPE element);
+TYPE linkedlist_popleft(LinkedList * linkedList);
+TYPE linkedlist_get(LinkedList * linkedList, int index);
+void linkedlist_insert(LinkedList * linkedList, TYPE element);
+void linkedlist_update(LinkedList * linkedList, TYPE element);
+TYPE linkedlist_delete(LinkedList * linkedList);
+int linkedlist_length(LinkedList * linkedList);
+
+#endif //DATA_STRUCTURE_WITH_C_DOUBLY_LINKED_LIST_H
