5728dfbc258b70a919ed66d22c1034782b13e246
Sijun Yang <yangsijun5528@gmail.com>
2024-06-16 14:14:35 +0900

    Heap 기능 정의 잘못된 부분 수정

--- include/heap.h
+++ include/heap.h
@@ -2,8 +2,8 @@
 // Created by sijun yang on 2024. 6. 15..
 //
 
-#ifndef DATA_STRUCTURE_WITH_C_HEAP_TREE_H
-#define DATA_STRUCTURE_WITH_C_HEAP_TREE_H
+#ifndef DATA_STRUCTURE_WITH_C_HEAP_H
+#define DATA_STRUCTURE_WITH_C_HEAP_H
 
 #define TYPE int // < 연산자 사용이 가능한 기본형만 Type으로 설정, 그래서 반환 시 포인터로 반환 안해도 됨.
 
@@ -13,7 +13,7 @@ extern int DEFAULT_INIT_CAPACITY;
 extern int RESIZE_SCALE;
 
 typedef struct {
-    TYPE *root;
+    TYPE *data;
     int capacity;
     int size;
 } Heap;
@@ -22,14 +22,16 @@ Heap heap_init();
 
 Heap heap_init_set_capacity(int init_capacity);
 
-void heap_push(TYPE data);
+void heap_push(Heap heap, TYPE data);
 
-TYPE heap_pop();
+TYPE heap_pop(Heap heap);
 
-int heap_get_size();
+int heap_get_size(Heap heap);
 
-bool heap_is_empty();
+bool heap_is_empty(Heap heap);
 
-TYPE heap_peek();
+TYPE heap_peek(Heap heap);
 
-#endif //DATA_STRUCTURE_WITH_C_HEAP_TREE_H
+void heap_free(Heap heap);
+
+#endif //DATA_STRUCTURE_WITH_C_HEAP_H
--- src/heap.c
+++ src/heap.c
@@ -2,18 +2,23 @@
 // Created by sijun yang on 2024. 6. 15. .
 //
 
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "heap.h"
 
 Heap heap_init() {};
 
 Heap heap_init_set_capacity(int init_capacity) {};
 
-void heap_push(TYPE data) {};
+void heap_push(Heap heap, TYPE data) {};
 
-TYPE heap_pop() {};
+TYPE heap_pop(Heap heap) {};
 
-int heap_get_size() {};
+int heap_get_size(Heap heap) {};
 
-bool heap_is_empty() {};
+bool heap_is_empty(Heap heap) {};
 
-TYPE heap_peek() {};
+TYPE heap_peek(Heap heap) {};
+
+void heap_free(Heap heap) {};
